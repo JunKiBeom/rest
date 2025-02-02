7ac34fa9dee01da4b9fcf515775b01ba506aa7ab
JunKiBeom <dona0408@gmail.com>
2025-02-03 02:07:45 +0900

    DELETE

--- src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
@@ -2,24 +2,29 @@ package com.example.rest.domain.post.post.controller;
 
 import com.example.rest.domain.post.post.entity.Post;
 import com.example.rest.domain.post.post.service.PostService;
-import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/posts")
+@RequiredArgsConstructor
 public class ApiV1PostController {
 
     private final PostService postService;
 
-    public ApiV1PostController(PostService postService) {
-        this.postService = postService;
-    }
-
     @GetMapping
-    @ResponseBody
     public List<Post> getItems() {
         return postService.getItems();
     }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable long id) {
+        Post post = postService.getItem(id).get();
+        postService.delete(post);
+
+        return "%d번 글 삭제가 완료되었습니다.".formatted(id);
+    }
+
 }
