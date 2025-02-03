9e1f8d74531b7d44a1e3900ae6d4c3ff4ddf4666
JunKiBeom <dona0408@gmail.com>
2025-02-04 03:06:47 +0900

    getItem, write 기능 구현

--- src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
@@ -22,13 +22,24 @@ public class ApiV1PostController {
         return postService.getItems();
     }
 
+
+    @GetMapping("{id}")
+    public Post getItem(@PathVariable long id) {
+        return postService.getItem(id).get();
+    }
+
+
     @DeleteMapping("/{id}")
     public RsData delete(@PathVariable long id) {
         Post post = postService.getItem(id).get();
         postService.delete(post);
 
-        return new RsData("200-1", "%d번 글 삭제가 완료되었습니다.".formatted(id));
+        return new RsData(
+                "200-1",
+                "%d번 글 삭제가 완료되었습니다.".formatted(id)
+        );
     }
+
 
     @AllArgsConstructor
     @Getter
@@ -37,12 +48,33 @@ public class ApiV1PostController {
         private String content;
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("{id}")
     public RsData modify(@PathVariable long id, @RequestBody ModifyForm form) {
+
         Post post = postService.getItem(id).get();
         postService.modify(post, form.getTitle(), form.getContent());
 
-        return new RsData("200-1", "%d번 글 수정이 완료되었습니다.".formatted(id));
+        return new RsData(
+                "200-1",
+                "%d번 글 수정이 완료되었습니다.".formatted(id)
+        );
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class WriteForm {
+        private String title;
+        private String content;
+    }
+
+    @PostMapping
+    public RsData write(@RequestBody WriteForm form) {
+        postService.write(form.getTitle(), form.getContent());
+
+        return new RsData(
+                "200-1",
+                "글 작성이 완료되었습니다."
+        );
     }
 
 }
